b5795f6a35b97482ed8d3c14cbf2a52865fd71ed
justinkiang <justin@justinkiang.com>
2021-01-03 14:15:15 -0800

    added security system

---
+++ accessory/security.go
@@ -0,0 +1,22 @@
+package accessory
+
+import (
+	"github.com/brutella/hc/service"
+)
+
+type SecuritySystem struct {
+	*Accessory
+	SecuritySystem *service.SecuritySystem
+}
+
+func NewSecuritySystem(info Info, currentValue int) *SecuritySystem {
+	acc := SecuritySystem{}
+	acc.Accessory = New(info, TypeSecuritySystem)
+	acc.SecuritySystem = service.NewSecuritySystem()
+
+	acc.SecuritySystem.SecuritySystemCurrentState.SetValue(currentValue)
+
+	acc.AddService(acc.SecuritySystem.Service)
+
+	return &acc
+}
